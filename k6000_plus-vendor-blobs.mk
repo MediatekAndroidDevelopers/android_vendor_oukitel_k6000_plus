d842d2a74de4e35b85b5385fab1d629dcedb1563
DerTeufel <dominik-kassel@gmx.de>
2017-11-01 20:06:28 +0100

    initial vendor blobs for k6k_plus

---
+++ k6000_plus-vendor-blobs.mk
@@ -0,0 +1,3 @@
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/oukitel/k6000_plus/usr,system/usr)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/oukitel/k6000_plus/vendor,system/vendor)
---
+++ k6000_plus-vendor.mk
@@ -0,0 +1,1 @@
+$(call inherit-product, vendor/oukitel/k6000_plus/k6000_plus-vendor-blobs.mk)
